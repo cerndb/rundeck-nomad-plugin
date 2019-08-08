12511c92c4eadf509d6f4220b61c280ad460ed80
Ignacio Coterillo <ignacio.coterillo@cern.ch>
2019-08-08 14:25:20 +0200

    WIP: NomadJobStepPlugin

--- src/main/java/io/github/valfadeev/rundeck/plugin/nomad/NomadJobStepPlugin.java
+++ src/main/java/io/github/valfadeev/rundeck/plugin/nomad/NomadJobStepPlugin.java
@@ -1,8 +1,10 @@
 package io.github.valfadeev.rundeck.plugin.nomad;
 
+import java.util.Collection;
 import java.util.Map;
 
 import com.dtolabs.rundeck.core.common.INodeEntry;
+import com.dtolabs.rundeck.core.common.INodeSet;
 import com.dtolabs.rundeck.core.execution.workflow.steps.StepException;
 import com.dtolabs.rundeck.core.execution.workflow.steps.node.NodeStepException;
 import com.dtolabs.rundeck.core.plugins.Plugin;
@@ -14,11 +16,15 @@ import com.dtolabs.rundeck.plugins.step.PluginStepContext;
 import com.dtolabs.rundeck.plugins.step.StepPlugin;
 import com.dtolabs.rundeck.plugins.util.DescriptionBuilder;
 import com.dtolabs.rundeck.plugins.util.PropertyBuilder;
+import com.hashicorp.nomad.apimodel.Node;
+import com.hashicorp.nomad.javasdk.NomadApiClient;
+import com.hashicorp.nomad.javasdk.NomadApiConfiguration;
 import com.dtolabs.rundeck.plugins.descriptions.PluginDescription;
 
 import io.github.valfadeev.rundeck.plugin.nomad.NomadNodeStepPlugin.Reason;
 import io.github.valfadeev.rundeck.plugin.nomad.common.Driver;
 import io.github.valfadeev.rundeck.plugin.nomad.common.SupportedDrivers;
+import io.github.valfadeev.rundeck.plugin.nomad.util.ParseInput;
 
 @Driver(name = SupportedDrivers.DOCKER)
 @Plugin(name = NomadJobStepPlugin.SERVICE_PROVIDER_NAME,
@@ -71,7 +77,29 @@ public class NomadJobStepPlugin implements StepPlugin, Describable {
         logger.log(2,"Example step extra config: " + configuration);
         logger.log(2,"Example step num: " + context.getStepNumber());
         logger.log(2,"Example step context: " + context.getStepContext());
-        logger.log(2, context.getNodes().toString());
+        
+        INodeSet Nodes = context.getNodes();
+        logger.log(5, Nodes.toString());
+        for (INodeEntry node : Nodes.getNodes()) {
+
+                logger.log(2, node.toString());
+
+                Map<String, String> attributes = node.getAttributes();
+
+                String nomadTokenStoragePath = attributes.get("token_path");
+                logger.log(5, "Accesing storage path " + nomadTokenStoragePath);
+                String nomadAuthToken = ParseInput.getPWFromStorage(context, nomadTokenStoragePath);
+
+                NomadApiConfiguration config =
+                        new NomadApiConfiguration
+                                .Builder()
+                                .setAddress(node.getAttributes().get("nomad_url"))
+                                .setAuthToken(nomadAuthToken)
+                                .build();
+                NomadApiClient apiClient = new NomadApiClient(config);
+
+        }
+
 
    }
 }
